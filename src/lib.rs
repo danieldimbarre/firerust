dba032d605cb302b83333ec4456e7f32dc3c5ad3
Daniel Dimbarre <49664655+danieldimbarre@users.noreply.github.com>
2022-04-13 19:49:14 -0300

    Firebase client

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+pub struct FirebaseClient {
+    url: String,
+    api_key: Option<String>,
+}
+
+impl FirebaseClient {
+    pub fn new(url: impl ToString) -> FirebaseClient {
+        FirebaseClient {
+            url: url.to_string(),
+            api_key: None,
+        }
+    }
+
+    pub fn auth(&mut self, api_key: &str) {
+        self.api_key = Some(api_key.to_string());
     }
 }

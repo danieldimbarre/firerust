4d89c054f192c1f55cdf93452d6c501bf2568ec8
Daniel Dimbarre <49664655+danieldimbarre@users.noreply.github.com>
2022-04-13 21:40:53 -0300

    Get data

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,8 +1,12 @@
-use std::error::Error;
 use firerust::FirebaseClient;
+use std::error::Error;
+use serde_json::Value;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let client = FirebaseClient::new(std::env::var("FIREBASE_URL")?);
-    
+    let client = FirebaseClient::new(std::env::var("FIREBASE_URL")?)?;
+    let reference = client.reference("/");
+
+    println!("{:?}", reference.get::<Value>()?);
+
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,27 @@
+use std::fmt::{ Display, Formatter, Debug };
+use serde::de::DeserializeOwned;
+use native_tls::TlsConnector;
+use std::io::{ Write, Read };
+use std::net::TcpStream;
+use std::error::Error;
+use serde::Serialize;
+use url::Url;
+
+
 #[derive(Debug, Clone)]
 pub struct FirebaseClient {
-    url: String,
+    url: Url,
+    connector: TlsConnector,
     api_key: Option<String>,
 }
 
 impl FirebaseClient {
-    pub fn new(url: impl ToString) -> FirebaseClient {
-        FirebaseClient {
-            url: url.to_string(),
+    pub fn new(url: impl ToString) -> Result<FirebaseClient, Box<dyn Error>> {
+        Ok(FirebaseClient {
             api_key: None,
-        }
+            connector: TlsConnector::new()?,
+            url: Url::parse(&url.to_string())?,
+        })
     }
 
     pub fn auth(&mut self, api_key: &str) {
@@ -32,5 +44,61 @@ impl RealtimeReference {
             client: client.clone(),
             path: path.to_string(),
         }
+    }
+
+    pub fn child(&self, path: impl ToString) -> RealtimeReference {
+        RealtimeReference::new(&self.client, format!("{}/{}", self.path, path.to_string()))
+    }
+
+    pub fn get<T>(&self) -> Result<T, Box<dyn Error>> where T: Serialize + DeserializeOwned + Debug {
+        let host = match self.client.url.domain() {
+            Some(host) => host,
+            None => match self.client.url.host_str() {
+                Some(host) => host,
+                None => return Err(Box::new(FirebaseError::new("Invalid URL")))
+            }
+        };
+
+        let port = match self.client.url.port_or_known_default() {
+            Some(port) => port,
+            None => return Err(Box::new(FirebaseError::new("Invalid URL")))
+        };
+
+        let mut buf = Vec::new();
+        let stream = TcpStream::connect(format!("{}:{}", host, port))?;
+        let mut stream = self.client.connector.connect(host, stream)?;
+
+        stream.write_all(format!("GET {}.json HTTP/1.0\r\nHost: {}\r\nAccept: application/json; charset=utf-8\r\n\r\n", self.path, host).as_bytes())?;
+        stream.read_to_end(&mut buf)?;
+
+        let response = String::from_utf8(buf)?;
+        let body = response.split("\r\n\r\n").collect::<Vec<&str>>()[1];
+
+        Ok(serde_json::from_str(body)?)
+    }
+}
+
+#[derive(Debug)]
+struct FirebaseError {
+    message: String
+}
+
+impl FirebaseError {
+    fn new(message: &str) -> FirebaseError {
+        FirebaseError {
+            message: message.to_string()
+        }
+    }
+}
+
+impl Error for FirebaseError {
+    fn description(&self) -> &str {
+        &self.message
+    }
+}
+
+impl Display for FirebaseError {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.message)
     }
 }
